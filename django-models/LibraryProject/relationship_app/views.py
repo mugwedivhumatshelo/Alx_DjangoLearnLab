ace0a34489cffc8df7d74c8f891252f522ac5397
mugwedivhumatshelo <mugwedivhumatshelo@gmail.com>
2025-02-23 12:24:27 -0800

    Implementing User Authentication in Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,40 +1,28 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-
-def login_view(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request=request, data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                messages.info(request, f"You are now logged in as {username}")
-                return redirect('list_books')
-            else:
-                messages.error(request, "Invalid username or password.")
-        else:
-messages.error(request, "Invalid username or password.")
-    form = AuthenticationForm()
-    return render(request, 'login.html', {'form': form})
-
-def logout_view(request):
-    logout(request)
-    messages.info(request, "You have successfully logged out.")
-    return render(request, 'logout.html')
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import AuthenticationForm
 
 def register_view(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            messages.success(request, "You have successfully registered.")
-            return redirect('list_books')
-        messages.error(request, "Unsuccessful registration. Invalid information.")
-    form = UserCreationForm()
-    return render(request, 'register.html', {'form': form})
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if get_user_model().objects.filter(username=username).exists():
+                messages.error(request, "Username already exists.")
+            elif get_user_model().objects.filter(email=email).exists():
+                messages.error(request, "Email already exists.")
+            else:
+                user = get_user_model().objects.create_user(username=username, email=email, password=password)
+                user.save()
+                login(request, user)
+                messages.success(request, "You have successfully registered.")
+                return redirect('list_books')
+        else:
+            messages.error(request, "Passwords do not match.")
+    return render(request, 'register.html')
 
