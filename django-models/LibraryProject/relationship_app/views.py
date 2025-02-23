d7761bda4d5a127e8659eb204eb36e47419f8d0d
mugwedivhumatshelo <mugwedivhumatshelo@gmail.com>
2025-02-23 12:32:03 -0800

    last

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,23 +7,13 @@ from django.contrib.auth.forms import UserCreationForm
 
 def register_view(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
-        password2 = request.POST['password2']
-
-        if password == password2:
-            if get_user_model().objects.filter(username=username).exists():
-                messages.error(request, "Username already exists.")
-            elif get_user_model().objects.filter(email=email).exists():
-                messages.error(request, "Email already exists.")
-            else:
-                user = get_user_model().objects.create_user(username=username, email=email, password=password)
-                user.save()
-                login(request, user)
-                messages.success(request, "You have successfully registered.")
-                return redirect('list_books')
-        else:
-            messages.error(request, "Passwords do not match.")
-    return render(request, 'register.html')
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "You have successfully registered.")
+            return redirect('list_books')
+        messages.error(request, "Unsuccessful registration. Invalid information.")
+    form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
 
