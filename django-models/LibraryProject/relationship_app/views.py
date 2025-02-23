3171f19b7b6f37f81b2c18b2123c5757147676f3
mugwedivhumatshelo <mugwedivhumatshelo@gmail.com>
2025-02-23 12:20:37 -0800

    Implementing User Authentication in Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,29 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}")
+                return redirect('list_books')
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    messages.info(request, "You have successfully logged out.")
+    return render(request, 'logout.html')
+
 def register_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
