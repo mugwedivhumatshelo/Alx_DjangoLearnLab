3051990d1888b1d68165092045ddeda8249fcf75
mugwedivhumatshelo <mugwedivhumatshelo@gmail.com>
2025-02-23 12:59:26 -0800

    last

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -20,15 +20,6 @@ def register_view(request):
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
 
-def admin_view(request):
-    return render(request, 'admin.html')
-
-def librarian_view(request):
-    return render(request, 'librarian.html')
-
-def member_view(request):
-    return render(request, 'member.html')
-
 def is_admin(user):
     return user.userprofile.role == 'Admin'
 
@@ -37,18 +28,18 @@ def is_librarian(user):
 
 def is_member(user):
     return user.userprofile.role == 'Member'
-from django.shortcuts import render
-from django.contrib.auth.decorators import user_passes_test
 
+@admin_view = user_passes_test(is_admin)
 def admin_view(request):
     return render(request, 'admin.html')
 
-def is_admin(user):
-    return user.userprofile.role == 'Admin'
+@librarian_view = user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'librarian.html')
 
-admin_view = user_passes_test(is_admin)(admin_view)
-librarian_view = user_passes_test(is_librarian)(librarian_view)
-member_view = user_passes_test(is_member)(member_view)
+@member_view = user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'member.html')
 
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import permission_required
