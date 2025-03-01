2b7909b269301505b2c38b957fd13ccda4a5af84
mugwedivhumatshelo <mugwedivhumatshelo@gmail.com>
2025-03-01 00:19:43 -0800

    Implement Role-Based Access Control in Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -42,19 +42,19 @@ from django.contrib.auth.decorators import user_passes_test
 def is_admin(user):
     return user.userprofile.role == 'Admin'
 
-def is_librarian(user):
-    return user.userprofile.role == 'Librarian'
-
-def is_member(user):
-    return user.userprofile.role == 'Member'
-
 @admin_view = user_passes_test(is_admin)
 def admin_view(request):
     return render(request, 'admin.html')
 
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
 @librarian_view = user_passes_test(is_librarian)
 def librarian_view(request):
     return render(request, 'librarian.html')
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
 
 @member_view = user_passes_test(is_member)
 def member_view(request):
