0a3a6d41999984e4368d793ae7523759a9b6e57f
mugwedivhumatshelo <mugwedivhumatshelo@gmail.com>
2025-03-01 01:13:38 -0800

    Implementing a Custom User Model in Django

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -103,3 +103,13 @@ class CustomUserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
+
+
