001f6bd18275dda0437fbc58dbd1c690fbf4d7d6
mugwedivhumatshelo <mugwedivhumatshelo@gmail.com>
2025-03-01 01:48:55 -0800

    Managing Permissions and Groups in Django

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render, redirect
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_detail(request, pk):
+    book = Book.objects.get(pk=pk)
+    return render(request, 'book_detail.html', {'book': book})
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_book(request):
+    if request.method == 'POST':
+        # Create book logic
+        return redirect('book_list')
+    return render(request, 'create_book.html')
+
+
