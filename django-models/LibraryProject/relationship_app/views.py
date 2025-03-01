25ac99560ac7be661e545a3bea925921739d293e
mugwedivhumatshelo <mugwedivhumatshelo@gmail.com>
2025-02-28 23:24:35 -0800

    Django Views and URL Configuration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,7 +7,7 @@ def list_books(request):
 
 from django.shortcuts import render
 from .models import Library
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 
 def list_books(request):
     books = Book.objects.all()
