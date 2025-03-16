414b61c2c5fa32bebc1300e095758118e0e656a8
mugwedivhumatshelo <mugwedivhumatshelo@gmail.com>
2025-03-16 07:48:00 -0700

    Implementing Filtering, Searching, and Ordering in Django REST Framework

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -66,6 +66,7 @@ class BookDeleteView(generics.DestroyAPIView):
 from rest_framework import generics
 from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
 from .models import Book
 from .serializers import BookSerializer
 
