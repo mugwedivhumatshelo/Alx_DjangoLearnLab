a335463b84575bd4c5298a4cfa793544a2c81cbb
mugwedivhumatshelo <mugwedivhumatshelo@gmail.com>
2025-03-23 13:01:43 -0700

    adding comments

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,7 +1,4 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 from django.contrib.auth.models import User
 
 class Post(models.Model):
@@ -11,5 +8,13 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
-        return self.title
+        returnf"{self.author.username} - {self.post.title}"
+
