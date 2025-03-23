0ead62f53fff4a1f9d1f46beb5c8864c626fb351
mugwedivhumatshelo <mugwedivhumatshelo@gmail.com>
2025-03-23 13:19:42 -0700

    updated commrnt update and delete

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -11,9 +11,8 @@ urlpatterns = [
     path('post/new/', views.PostCreateView.as_view(), name='post_create'),
     path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post_update'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
-path('posts/<int:post_id>/comments/', views.CommentListView.as_view(), name='comment_list'),
-    path('posts/<int:post_id>/comments/new/', views.CommentCreateView.as_view(), name='comment_create'),
-    path('posts/<int:post_id>/comments/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment_update'),
-    path('posts/<int:post_id>/comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete')
+    path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment_create'),
+    path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment_update'),
+    path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),
 ]
 
